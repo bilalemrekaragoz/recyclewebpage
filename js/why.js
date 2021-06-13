c00c7623c48a105280c830a4f06e694f3b2edf3c
bilalemrekaragoz <66293987+bilalemrekaragoz@users.noreply.github.com>
2021-06-13 21:53:41 +0300

    Update

---
+++ js/why.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    
+   var text = "";
+
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.overrideMimeType("application/json");
+		
+	ourRequest.open('GET','why.json');
+
+	ourRequest.onload=function(){
+		var ourData=JSON.parse(ourRequest.responseText);
+        textCreator(ourData);
+	};
+	ourRequest.send();
+
+    function textCreator(ourData)
+    {
+        for(i=0 ; i<ourData.length; i++)
+        {
+            text += '<h3>' + ourData[i].title + '</h3>' +
+            '<div>' +  '<p>' + ourData[i].text + '</p> </div>'
+        }
+        $("#acordion").html(text);
+    }
+
+});
