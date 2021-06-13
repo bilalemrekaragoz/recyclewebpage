f527c57508dc7a7897a802e4c5fff7fcf537d042
bilalemrekaragoz <66293987+bilalemrekaragoz@users.noreply.github.com>
2021-06-13 18:20:05 +0300

    Update

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,19 +1,27 @@
+$(document).ready(function(){
+    
+   
 
-	
-window.onload = function () {
 	var ourRequest = new XMLHttpRequest();
-		ourRequest.overrideMimeType("application/json");
-			 
-		ourRequest.open('GET','https://trackrecycle.herokuapp.com/aboutus-api.php');
+	ourRequest.overrideMimeType("application/json");
+		
+	ourRequest.open('GET','https://trackrecycle.herokuapp.com/aboutus-api.php');
 
-		ourRequest.onload=function(){
-			var ourData=JSON.parse(ourRequest.responseText);
-			for(i=0 ; i<ourData.lenght; i++)
-			{
-				var imgID = "#img"+i;
-				$(imgID).attr("src",ourData[i].image);
-			}
-		};
-		ourRequest.send();	
+	ourRequest.onload=function(){
+		var ourData=JSON.parse(ourRequest.responseText);
+		for(i=0 ; i<ourData.length; i++)
+		{
+			$("#img3").attr("src",ourData[i].image);
+			$("p").text(ourData[i].text);
+			console.log(ourData[i].text);
+		}
+	};
+	ourRequest.send();
 
-};
+
+});
+
+
+
+
+
