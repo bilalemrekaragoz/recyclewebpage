7c558b2c5ecb3a2d3ce98b70d7a2307ad8716b1b
bilalemrekaragoz <66293987+bilalemrekaragoz@users.noreply.github.com>
2021-06-13 17:59:25 +0300

    Update

---
+++ js/aboutus.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	
+		var ourRequest = new XMLHttpRequest();
+		ourRequest.overrideMimeType("application/json");
+			 
+		ourRequest.open('GET','./aboutus-api.php');
+
+		ourRequest.onload=function(){
+			var ourData=JSON.parse(ourRequest.responseText);
+			for(i=0 ; i<ourData.lenght; i++)
+			{
+				var imgID = "#img"+i;
+				$(imgID).attr("src",ourData[i].image);
+			}
+		};
+		ourRequest.send();	
+
+	
+	}); // end ready
